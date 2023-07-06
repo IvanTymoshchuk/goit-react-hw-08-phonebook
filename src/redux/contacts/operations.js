23fddaf98281724f86004cb783da9f2a0cf0c378
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-07-06 22:25:03 +0200

    Update operations.js

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://6443bb28466f7c2b4b593743.mockapi.io';
+// axios.defaults.baseURL = 'https://6443bb28466f7c2b4b593743.mockapi.io';
 
 export const fetchAll = createAsyncThunk(
   'contacts/fetchAll',
