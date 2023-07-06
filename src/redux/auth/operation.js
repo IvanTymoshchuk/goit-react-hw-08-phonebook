040da9cedabd313edc2e4f7d9839f21596af0b86
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-07-06 22:39:12 +0200

    Update operation.js

--- src/redux/auth/operation.js
+++ src/redux/auth/operation.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
