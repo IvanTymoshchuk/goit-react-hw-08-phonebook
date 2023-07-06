61ef8233b0a5f85db14265ce54ab330deccbe578
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-07-06 13:26:18 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,3 @@
-// import { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { ToastContainer } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import { fetchAll } from 'redux/contacts/operations';
-// import {Layout} from './Layout/Layout';
-// import GlobalTitle from './Layout/Title';
-// import FormList from './FormList/FormList';
-// import ContactList from './ContactList/ContactList';
-// import Filter from './Filter/Filter';
-// import { Route, Routes } from 'react-router-dom';
-// import Home from '../pages/Home'
-
-// function App() {
-//   // const dispatch = useDispatch();
-
-//   // useEffect(() => {
-//   //   dispatch(fetchAll());
-//   // }, [dispatch]);
-
-//   return (
-//     <Layout>
-//       <Routes>
-//         <Route  index element={<Home />}/>
-//       </Routes>
-//       {/* <GlobalTitle title="Phonebook" />
-//       <FormList />
-//       <GlobalTitle title="Contacts" />
-//       <Filter />
-//       <ContactList />
-//       <ToastContainer /> */}
-//     </Layout>
-//   );
-// }
-
-// export default App;
 import { useEffect, lazy } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
