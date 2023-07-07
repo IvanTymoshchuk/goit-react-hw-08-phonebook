484e5effdc597edd1c286a7557dcd7c0d5ade46c
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-07-07 15:26:22 +0200

    css

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,17 +1,19 @@
 import { useDispatch } from 'react-redux';
-import { logOut } from '../../redux/auth/operation';
+import { logOut } from 'redux/auth/operation';
 import { useAuth } from 'hooks';
+import { Button } from '../FormList/FormList.styled';
+import {Container,Title} from './UserMenu.styled'
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
   const { user } = useAuth();
 
   return (
-    <div>
-      <p>Welcome, {user.name}</p>
-      <button type="button" onClick={() => dispatch(logOut())}>
+    <Container>
+      <Title>Welcome, {user.name}</Title>
+      <Button type="button" onClick={() => dispatch(logOut())}>
         Logout
-      </button>
-    </div>
+      </Button>
+    </Container>
   );
 };
---
+++ src/components/UserMenu/UserMenu.styled.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Title = styled.h2`
+  color: white;
+`;
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+`;
