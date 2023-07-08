6c4117a525343245a5e5d4ba54bf6a3e58941fc1
Ivan Tymoshchuk <timohin2231@gmail.com>
2023-07-08 09:51:25 +0200

    Update Contacts.jsx

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,10 +1,18 @@
 import { HelmetProvider } from 'react-helmet-async';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchAll } from 'redux/contacts/operations';
 import FormList from '../components/FormList/FormList';
 import ContactList from '../components/ContactList/ContactList';
 import Filter from '../components/Filter/Filter';
 import GlobalTitle from '../components/Layout/Title';
 
 function Contacts() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAll());
+  }, [dispatch]);
   return (
     <>
       <HelmetProvider>
